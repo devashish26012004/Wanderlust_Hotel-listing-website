0853d7cf835492e9db800084ea0d975dc3e51c27
Devashish Dumbhare <devashishimp26@gmail.com>
2024-08-28 19:57:32 +0530

    change in review controller

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -6,11 +6,9 @@ module.exports.createReview = async (req, res) => {
   let listing = await Listing.findById(id);
   let newReview = new Review(req.body.review);
   newReview.author = req.user._id;
-  console.log(newReview);
   listing.reviews.push(newReview);
   await newReview.save();
   await listing.save();
-
   req.flash("success", "New Review Created!");
   res.redirect(`/listings/${id}`);
 };
